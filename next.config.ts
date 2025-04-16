305df9bbfe1573915ce128622e9b3e95c62465c3
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2025-04-16 15:36:11 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import path from 'path'
 import type { NextConfig } from 'next'
-
+ 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    return config
+  turbopack: {
+    // ...
   },
 }
-
+ 
 export default nextConfig
